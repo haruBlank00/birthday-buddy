443e7c0f516c5484d92efd17018f6873e93aa168
haruBlank <harublank01@gmail.com>
2024-04-16 21:34:33 +0545

    install date-fns calculate age and generate age string

--- src/components/birthday-card/index.tsx
+++ src/components/birthday-card/index.tsx
@@ -1,4 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { differenceInYears } from "date-fns";
 
 type TBirthdayCardProps = {
   name: string;
@@ -6,6 +7,9 @@ type TBirthdayCardProps = {
   image: string;
 };
 export const BirthdayCard = ({ name, dob, image }: TBirthdayCardProps) => {
+  const age = differenceInYears(new Date(), dob);
+  const ageString = age > 1 ? `${age} years` : `${age} year`;
+
   return (
     <div className="shadow-md p-4 flex items-center gap-x-4 hover:scale-105 transition-transform cursor-pointer">
       <Avatar className="h-16 w-16">
@@ -14,7 +18,7 @@ export const BirthdayCard = ({ name, dob, image }: TBirthdayCardProps) => {
       </Avatar>
       <div>
         <p className="text-2xl capitalize font-semibold">{name}</p>
-        <p className="text-gray-400">{dob.toLocaleDateString()}</p>
+        <p className="text-gray-400">{ageString}</p>
       </div>
     </div>
   );
