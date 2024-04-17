c6900796ca86520cf5791afb71e20b32e70b7d6f
haruBlank <harublank01@gmail.com>
2024-04-17 10:07:59 +0545

    fix ts error

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,11 +1,11 @@
-export const useLocalStorage = <T, K>() => {
+export const useLocalStorage = <T, K extends string>() => {
   const setItem = (key: K, value: T) => {
     const stringifyedValue = JSON.stringify(value);
     window.localStorage.setItem(key, stringifyedValue);
   };
   const getItem = (key: K): T | null => {
     try {
-      const item = window.localStorage.getItem(key as string);
+      const item = window.localStorage.getItem(key);
       if (item) {
         return JSON.parse(item);
       }
